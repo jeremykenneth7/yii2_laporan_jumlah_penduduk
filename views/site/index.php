ed439d014a527af66a2e9061626ee72e38ab7ee6
jeremykenneth7 <jeremykenneth7@gmail.com>
2024-06-26 08:42:47 +0700

    first commit

---
+++ views/site/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Laporan Jumlah Penduduk</h1>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3 mt-5">
+                <h2>Data Nama Provinsi</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Data Nama Provinsi &raquo;</a></p>
+            </div>
+            <div class="col-lg-4 mb-3 mt-5">
+                <h2>Data Kabupaten</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Data Kabupaten &raquo;</a></p>
+            </div>
+            <div class="col-lg-4 mb-3 mt-5">
+                <h2>Data Penduduk</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Data Penduduk &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
