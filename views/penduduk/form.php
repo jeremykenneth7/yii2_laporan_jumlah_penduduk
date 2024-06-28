aa6ecf1aff0f8e6e856dd667b53a287ff9e0e6ff
jeremykenneth7 <jeremykenneth7@gmail.com>
2024-06-28 08:42:12 +0700

    fix form submit kabupaten n provinsi

--- views/penduduk/form.php
+++ views/penduduk/form.php
@@ -20,12 +20,15 @@ $provinsiOptions = Provinsi::find()
 $provinsiOptions = \yii\helpers\ArrayHelper::map($provinsiOptions, 'id_provinsi', 'nama_provinsi');
 
 $kabupatenOptions = Kabupaten::find()
-    ->select(['id_kabupaten', 'nama_kabupaten'])
+    ->select(['id_kabupaten', 'nama_kabupaten', 'id_provinsi'])
     ->orderBy('nama_kabupaten')
     ->asArray()
     ->all();
 
-$kabupatenOptions = \yii\helpers\ArrayHelper::map($kabupatenOptions, 'id_kabupaten', 'nama_kabupaten');
+$kabupatenOptionsArray = [];
+foreach ($kabupatenOptions as $kabupaten) {
+    $kabupatenOptionsArray[$kabupaten['id_provinsi']][] = $kabupaten;
+}
 ?>
 
 <style>
@@ -65,20 +68,64 @@ $kabupatenOptions = \yii\helpers\ArrayHelper::map($kabupatenOptions, 'id_kabupat
 </div>
 
 <div class="row">
-    <?= $form->field($model, 'alamat')->textarea()->label('Alamat') ?>
+    <?= $form->field($model, 'alamat')->textarea(['id' => 'alamat-field'])->label('Alamat') ?>
 </div>
 
 <div class="row">
-    <?= $form->field($model, 'id_provinsi')->dropDownList($provinsiOptions, ['prompt' => 'Select Provinsi'])->label('Provinsi') ?>
+    <?= $form->field($model, 'id_provinsi')->dropDownList($provinsiOptions, [
+        'prompt' => 'Select Provinsi',
+        'id' => 'provinsi-field',
+        'onchange' => 'updateKabupatenOptions()'
+    ])->label('Provinsi') ?>
 </div>
 
 <div class="row">
-    <?= $form->field($model, 'id_kabupaten')->dropDownList($kabupatenOptions, ['prompt' => 'Select Kabupaten'])->label('Kabupaten') ?>
+    <?= $form->field($model, 'id_kabupaten')->dropDownList([], ['prompt' => 'Select Kabupaten', 'id' => 'kabupaten-field'])->label('Kabupaten') ?>
 </div>
 
 <div class="modal-footer p-0">
-    <?= Html::resetButton('Batal', ['form', 'class' => 'btn btn-secondary', 'data-bs-dismiss' => "modal"]) ?>
-    <?= Html::submitButton('Simpan', ['form', 'class' => 'btn btn-primary']) ?>
+    <?= Html::resetButton('Batal', ['class' => 'btn btn-secondary', 'data-bs-dismiss' => "modal"]) ?>
+    <?= Html::submitButton('Simpan', ['class' => 'btn btn-primary']) ?>
 </div>
 
 <?php ActiveForm::end() ?>
+
+<?php
+$kabupatenJsOptions = json_encode($kabupatenOptionsArray);
+
+$script = <<<JS
+    function updateKabupatenOptions() {
+        var provinsiField = document.getElementById('provinsi-field');
+        var kabupatenField = document.getElementById('kabupaten-field');
+        var kabupatenOptions = $kabupatenJsOptions;
+        
+        kabupatenField.innerHTML = '<option value="">Select Kabupaten</option>';
+        
+        if (provinsiField.value && kabupatenOptions[provinsiField.value]) {
+            kabupatenOptions[provinsiField.value].forEach(function(kabupaten) {
+                var option = document.createElement('option');
+                option.value = kabupaten.id_kabupaten;
+                option.text = kabupaten.nama_kabupaten;
+                kabupatenField.add(option);
+            });
+        }
+    }
+    
+    document.getElementById('form-elem').addEventListener('submit', function(event) {
+        var alamatField = document.getElementById('alamat-field');
+        var provinsiField = document.getElementById('provinsi-field');
+        var kabupatenField = document.getElementById('kabupaten-field');
+        
+        var selectedProvinsi = provinsiField.options[provinsiField.selectedIndex].text;
+        var selectedKabupaten = kabupatenField.options[kabupatenField.selectedIndex].text;
+        
+        if (provinsiField.value && kabupatenField.value) {
+            alamatField.value += "\\n" + selectedProvinsi + ", " + selectedKabupaten;
+        }
+    });
+
+    updateKabupatenOptions();
+JS;
+
+$this->registerJs($script);
+?>
