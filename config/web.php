4a9d587efccc6e4f07ed424acfbcde0704c3445c
jeremykenneth7 <jeremykenneth7@gmail.com>
2024-06-29 15:03:20 +0700

    yii debug to prod

--- config/web.php
+++ config/web.php
@@ -59,21 +59,26 @@ $config = [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
+if (getenv('YII_ENV') === 'dev') {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (getenv('YII_DEBUG')) {
+        $config['bootstrap'][] = 'debug';
+        $config['modules']['debug'] = [
+            'class' => 'yii\debug\Module',
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
+        ];
+    }
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+    if (getenv('YII_MODULE_GII')) {
+        $config['bootstrap'][] = 'gii';
+        $config['modules']['gii'] = [
+            'class' => 'yii\gii\Module',
+            // uncomment the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
+        ];
+    }
 }
+
 
 return $config;
